725a654385bc48dc014cf0ba7243a31357551699
Yan Wang <wangyan6783@gmail.com>
2018-06-14 00:55:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find{|item| cheese_types.include?(item) == true}
+  array.find{|item| cheese_types.include?(item)}
 end
