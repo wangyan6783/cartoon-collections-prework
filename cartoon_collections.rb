16f3e1284495ae7c620d1aac6ed1fb51a4c2788b
Yan Wang <wangyan6783@gmail.com>
2018-06-14 00:44:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(names)
   names.each_with_index{|name, i| puts "#{i+1} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect{|call| "#{call.upcase}!"}
 end
 
 def long_planeteer_calls# code an argument here
