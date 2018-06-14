a1f79d76d89cde6bb10e795d93fe4b46b73bce97
Yan Wang <wangyan6783@gmail.com>
2018-06-14 00:47:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(array)
   array.collect{|call| "#{call.upcase}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any?{|call| call.size > 4}
 end
 
 def find_the_cheese# code an argument here
